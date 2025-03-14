defd673b14a0733706df92ed87f6a42d6fecb39d
youssef-medhatt <youssefmedhatt2001@gmail.com>
2025-03-14 14:53:30 +0200

    stripe changes

--- Backend/helpers/stripe.js
+++ Backend/helpers/stripe.js
@@ -4,5 +4,6 @@ import Stripe from 'stripe';
 
 dotenv.config();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+console.log('Stripe Key:', process.env.STRIPE_SECRET_KEY ? 'Exists' : 'Not Found');
 
 export default stripe;
